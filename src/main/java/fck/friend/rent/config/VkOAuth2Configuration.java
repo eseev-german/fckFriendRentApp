b1dfde933349ab128e71150dbb0b1c66a3c80104
geseev <german.eseev@t-systems.com>
2019-11-26 17:45:56 +0300

    get current user vk friends

--- src/main/java/fck/friend/rent/config/VkOAuth2Configuration.java
+++ src/main/java/fck/friend/rent/config/VkOAuth2Configuration.java
@@ -1,6 +1,9 @@
 package fck.friend.rent.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fck.friend.rent.converter.VkOAuth2UserRequestEntityConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
@@ -15,10 +19,12 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Objects;
 
 @Configuration
 public class VkOAuth2Configuration {
@@ -29,25 +35,42 @@ public class VkOAuth2Configuration {
         return defaultOAuth2UserService;
     }
 
-    @Bean
-    public RestTemplate restTemplate(OAuth2AuthorizedClientService clientService) {
+    @Bean(name = "vkRestTemplate")
+    public RestTemplate vkRestTemplate(OAuth2AuthorizedClientService clientService) {
         return new RestTemplateBuilder()
-                .interceptors(createAddAccessTokenToRequest(clientService))
+                .interceptors(addVkParams(clientService))
+                .uriTemplateHandler(new DefaultUriBuilderFactory(baseUrl))
+                .messageConverters(mappingJackson2HttpMessageConverter())
                 .build();
     }
 
-    private ClientHttpRequestInterceptor createAddAccessTokenToRequest(OAuth2AuthorizedClientService clientService) {
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper());
+        return converter;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+
+        return mapper;
+    }
+
+    private ClientHttpRequestInterceptor addVkParams(OAuth2AuthorizedClientService clientService) {
         return (request, body, execution) -> {
             Authentication authentication = SecurityContextHolder.getContext()
                                                                  .getAuthentication();
             if (authentication instanceof OAuth2AuthenticationToken) {
-                request = addAccessTokenToRequest(clientService, request, (OAuth2AuthenticationToken) authentication);
+                request = addVkParams(clientService, request, (OAuth2AuthenticationToken) authentication);
             }
             return execution.execute(request, body);
         };
     }
 
-    private HttpRequest addAccessTokenToRequest(OAuth2AuthorizedClientService clientService, HttpRequest request, OAuth2AuthenticationToken authentication) throws IOException {
+    private HttpRequest addVkParams(OAuth2AuthorizedClientService clientService, HttpRequest request, OAuth2AuthenticationToken authentication) throws IOException {
         OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
         String tokenValue = client.getAccessToken()
                                   .getTokenValue();
